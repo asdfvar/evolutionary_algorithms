d1cd7347a85be15e9f5025a5fd2b16ddc4754b61
asdfvar <asdfvar@gmail.com>
2014-09-13 14:32:57 -0700

    first initially working version

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -17,22 +17,23 @@ def mixWeights(weights1, weights2):
       elif ZeroOrOne == 1:
          weight = weights2[i]
       newWeights[i] = weight
+   # re-normalize the weights
    newWeights += np.random.randn(len(newWeights))*nu
    newWeights -= min(newWeights)
    newWeights /= (max(newWeights) - min(newWeights))
    return newWeights
    
 
-N_players = 8
+N_players = 24
 
 players = []
 for ii in range(N_players):
    players.append(player.player())
 
-for i_game in range(10):
+for i_game in range(100):
+   print "\n\nRound %d" % i_game
    random.shuffle(players)
 
-   print len(players)
    # have each compete
    for i_player in range(0,len(players), 2):
       player1 = i_player
@@ -72,28 +73,43 @@ for i_game in range(10):
             play = players[player2].play(possible_plays)
             brd.play2(play)
 
-      print brd.board
       if brd.win(1):
          print "player 1 wins"
          players[player1].win  = True
          players[player2].lose = True
-      if brd.win(2):
+      elif brd.win(2):
          print "player 2 wins"
          players[player1].lose = True
          players[player2].win  = True
+      else:
+         print "DRAW"
+      print brd.board
 
    # kill losers and breed winners
    Losers = []
    Winners = []
-   for i_player,plyr in enumerate(players):
-      if plyr.lose == True:
-         print "poping"
-         Losers.append(players.pop(i_player))
-      elif plyr.win == True:
-         Winners.append(players[i_player])
+   for i in range(len(players)-1,-1,-1):
+      if players[i].lose == True:
+         Losers.append(players.pop(i))
+      elif players[i].win == True:
+         Winners.append(players[i])
 
    # reset winners
    for i_player,plyr in enumerate(players):
       players[i_player].win = False
       players[i_player].lose = False
 
+   for i in range(len(Winners)):
+      Winners[i].win = False
+      Winners[i].lose = False
+
+   for i in range(len(Losers)):
+      Losers[i].win = False
+      Losers[i].lose = False
+
+   while len(players) < N_players:
+      winner1 = random.choice(Winners)
+      winner2 = random.choice(Winners)
+      newPlayer = player.player()
+      newPlayer.weights = mixWeights(winner1.weights, winner2.weights)
+      players.append(newPlayer)
